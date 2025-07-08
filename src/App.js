964ae578c7cfdfd0a1f59d329ee27972f5d944e5
Monika-msk <monikashivakumar05@gmail.com>
2025-07-08 13:58:49 +0530

    Updated route for Dhanbad–Ranchi–Jamshedpur via Ranchi

--- src/App.js
+++ src/App.js
@@ -19,7 +19,15 @@ const corridorDefs = [
 { id: 'M', name: 'Ahmedabad – Mundra', src: [72.5714, 23.0225], dst: [69.7190, 22.8392], color: '#6C5CE7' },
 { id: 'N', name: 'Vijayawada – Hyderabad', src: [80.6480, 16.5062], dst: [78.4867, 17.3850], color: '#FAB1A0' },
 { id: 'O', name: 'Mumbai – Nashik', src: [72.8777, 19.0760], dst: [73.7898, 19.9975], color: '#81ECEC' },
-{ id: 'P', name: 'Dhanbad – Ranchi – Jamshedpur', src: [86.4304, 23.7957], dst: [86.2029, 22.8046], color: '#FD79A8' },
+{
+  id: 'P',
+  name: 'Dhanbad – Ranchi – Jamshedpur',
+  src: [86.4304, 23.7957],             // Dhanbad
+  mid: [85.3333, 23.3441],             // Ranchi (waypoint)
+  dst: [86.2029, 22.8046],             // Jamshedpur
+  color: '#FD79A8'
+
+},
 { id: 'Q', name: 'Pune – Kolhapur', src: [73.8567, 18.5204], dst: [74.2333, 16.7050], color: '#74B9FF' },
 { id: 'R', name: 'Surat – Vadodara', src: [72.8311, 21.1702], dst: [73.1812, 22.3072], color: '#D63031' },
 { id: 'S', name: 'Hubballi – Chitradurga', src: [75.1240, 15.3647], dst: [76.4039, 14.2222], color: '#00B894' },
@@ -594,7 +602,12 @@ export default function EVMapDashboard() {
 
   useEffect(() => {
     corridorDefs.forEach(c => {
-      const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${c.src[0]},${c.src[1]};${c.dst[0]},${c.dst[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
+      const coords = c.mid
+  ? `${c.src[0]},${c.src[1]};${c.mid[0]},${c.mid[1]};${c.dst[0]},${c.dst[1]}`
+  : `${c.src[0]},${c.src[1]};${c.dst[0]},${c.dst[1]}`;
+  const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${coords}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
+
+
       fetch(url)
         .then(res => res.json())
         .then(json => {
