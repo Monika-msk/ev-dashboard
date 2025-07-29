2992bbc4f2947e5362041aa3eb60e041e619ff7f
Monika-msk <monikashivakumar05@gmail.com>
2025-07-29 20:49:16 +0530

    Commit message here

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders map search input', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Check for search input by placeholder text
+  const searchInput = screen.getByPlaceholderText(/search corridor id, name, or site id/i);
+  expect(searchInput).toBeInTheDocument();
 });
